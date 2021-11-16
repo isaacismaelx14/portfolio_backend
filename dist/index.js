5a117502c796ca4fd8f44fcbe8f3a02ef0237315
Isaac Martinez <isaacismaelx14@gmail.com>
2021-11-16 01:22:10 -0400

    Production #

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var server_1 = __importDefault(require("./server"));
 function main() {
-    var server = new server_1.default("v1.0.0  ⭐");
+    var server = new server_1.default("v1.0.0 #");
     server.start();
 }
 main();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Server from "./server";
 
 function main(): void {
-  const server = new Server("v1.0.0");
+  const server = new Server("v1.0.0 #");
   server.start();
 }
 
